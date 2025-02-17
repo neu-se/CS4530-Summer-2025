eec6e1b33d13575215eaf90073bd133f7563bef1
Rob Simmons <robsimmons@gmail.com>
2025-02-17 09:08:57 -0500

    Fix base url

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: "/CS4530-Summer-2025",
+  trailingSlash: "never",
+});
