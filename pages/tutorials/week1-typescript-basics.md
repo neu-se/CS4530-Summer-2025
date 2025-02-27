56f4d99ba76715c601997f137f52771ed9b6b79e
Rob Simmons <robsimmons@gmail.com>
2025-02-26 23:28:10 -0500

    Working navigation setup

---
+++ pages/tutorials/week1-typescript-basics.md
@@ -0,0 +1,8 @@
+---
+layout: page
+title: Typescript Basics
+nav_order: 2
+---
+
+This tutorial describes the basic concepts and syntax of typescript with code examples. Typescript is treated here as an extension of JavaScript, but it requires no prior knowledge to follow along.
+
--- src/content.config.ts
+++ src/content.config.ts
@@ -5,8 +5,16 @@ import { glob } from "astro/loaders";
 const pages = defineCollection({
   loader: glob({ pattern: "**/*.(md|mdx)", base: "./pages" }),
   schema: z.discriminatedUnion("layout", [
-    z.object({ layout: z.literal("home"), title: z.string() }),
-    z.object({ layout: z.literal("schedule"), title: z.string() }),
+    z.object({
+      layout: z.literal("home"),
+      title: z.string(),
+      nav_order: z.optional(z.number()),
+    }),
+    z.object({
+      layout: z.literal("schedule"),
+      title: z.string(),
+      nav_order: z.optional(z.number()),
+    }),
     z.object({
       layout: z.literal("page"),
       title: z.string(),
